cd0ca2c64330dc0ba220cd03b38dd0b1c9006f16
Наталья Бачурина <natali.gorobets.86@mail.ru>
2022-04-26 15:50:41 +0700

    добавлены файлы

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 1; // сколько будет миль
+
+        int cost = 100; // рублей стоимость билета
+        int amount = 20; // за это количество рублей
+
+        System.out.println("Вам начислено:");
+        System.out.println((cost / amount) + " миль");
+    }
+}
